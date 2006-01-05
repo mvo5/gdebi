52ef286a150bf6a4f61ce8e7f6d243ee9cebcbab
Gustavo Franco <stratus@debian.org>
2006-01-05 11:50:35 -0200

    apt_inst.debExtractControl better error handling added

--- GDebi/DebPackage.py
+++ GDebi/DebPackage.py
@@ -14,7 +14,10 @@ class DebPackage:
         self.file = file
         self._needPkgs = None
         # read the deb
-        control = apt_inst.debExtractControl(open(file))
+        try:
+            control = apt_inst.debExtractControl(open(file))
+        except SystemError:
+            print "error extracting the control information, not a valid deb file"
         self._sections = apt_pkg.ParseSection(control)
         self.pkgName = self._sections["Package"]
 
