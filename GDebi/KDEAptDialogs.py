e4f3efc76670179385e34acf9ff00a6824c049c1
Jonathan Riddell <jriddell@canonical.com>
2008-07-30 19:21:34 +0100

    consistent 4 space indenting for KDEAptDialogs.py

--- GDebi/KDEAptDialogs.py
+++ GDebi/KDEAptDialogs.py
@@ -61,111 +61,109 @@ def utf8(str):
 class KDEDpkgInstallProgress(object):
     # this one is the frontend for dpkg -i
     # there is only 0/100 state for the progress bar
-    
+
     def __init__(self, debfile, status, progress, konsole, parent):
-	# an expander would be handy, sadly we don't have one in KDE3
-	self.debfile = debfile
-	self.status = status
-	self.progress = progress
-	self.konsole = konsole
-	self.parent = parent
-	
-	# in case there was some progress left from the deps
-	self.progress.setValue(0)
+        # an expander would be handy, sadly we don't have one in KDE3
+        self.debfile = debfile
+        self.status = status
+        self.progress = progress
+        self.konsole = konsole
+        self.parent = parent
+
+        # in case there was some progress left from the deps
+        self.progress.setValue(0)
     def timeoutHandler(self,signum, frame):
         # print 'Stopped waiting for I/O ', signum
         raise IOError, "Stopped waiting for I/O."
 
     def commit(self):
-	# ui
-	print "type: " + str(type(self.debfile))
-	self.status.setText(_("Installing '%s'...") % \
-				os.path.basename(self.debfile))
-	# the command
-	cmd = "/usr/bin/dpkg"
-	argv = [cmd, "-i", self.debfile]
-	(rNum,wNum) = os.pipe()
-	self.child_pid = os.fork()
-	
-	if self.child_pid == 0:
-		# we're the child, call a subprocess, wait for the exit status, use the parent Konsole fd's as stdin/stdout
-		exitstatus = subprocess.call(argv,stdin=self.parent.master, stdout=self.parent.slave,stderr=subprocess.STDOUT)
-                #exitstatus = subprocess.call(argv)
-		os.write(wNum,str(exitstatus))
-		os._exit(0)
-	else:
-		self.exitstatus = -5
-		while self.exitstatus < 0:
-			# okay, so what we're doing now:
-			# we're using the select check to see if the pipe is readable. If it is, we're reading it
-			# see select(2) or select at libref for more info
-			# TODO: implement error checking
-			while True:
-				try:
-					(rlist, wlist, xlist) = select.select([self.parent.master],[],[], 0)
-					if len(rlist) > 0:
-						line = os.read(self.parent.master, 255)
-						#self._terminal_log.write(line)
-						print "inserting line: " + line
-						self.parent.konsole.insertWithTermCodes(utf8(line))
-					else:
-						break
-				except Exception, e:
-					print e
-					#logging.debug("error reading from self.master_fd '%s'" % e)
-				break
+        # ui
+        print "type: " + str(type(self.debfile))
+        self.status.setText(_("Installing '%s'...") % os.path.basename(self.debfile))
+        # the command
+        cmd = "/usr/bin/dpkg"
+        argv = [cmd, "-i", self.debfile]
+        (rNum,wNum) = os.pipe()
+        self.child_pid = os.fork()
 
-			
-			try:
-				readable = select.select([rNum],[],[],0.001)
-				if len(readable[0]) > 0:
-					self.exitstatus = int(os.read(rNum,2))
+        if self.child_pid == 0:
+            # we're the child, call a subprocess, wait for the exit status, use the parent Konsole fd's as stdin/stdout
+            exitstatus = subprocess.call(argv,stdin=self.parent.master, stdout=self.parent.slave,stderr=subprocess.STDOUT)
+            #exitstatus = subprocess.call(argv)
+            os.write(wNum,str(exitstatus))
+            os._exit(0)
+        else:
+            self.exitstatus = -5
+            while self.exitstatus < 0:
+                # okay, so what we're doing now:
+                # we're using the select check to see if the pipe is readable. If it is, we're reading it
+                # see select(2) or select at libref for more info
+                # TODO: implement error checking
+                while True:
+                    try:
+                        (rlist, wlist, xlist) = select.select([self.parent.master],[],[], 0)
+                        if len(rlist) > 0:
+                            line = os.read(self.parent.master, 255)
+                            #self._terminal_log.write(line)
+                            print "inserting line: " + line
+                            self.parent.konsole.insertWithTermCodes(utf8(line))
+                        else:
+                            break
+                    except Exception, e:
+                        print e
+                        #logging.debug("error reading from self.master_fd '%s'" % e)
+                    break
 
-			except (OSError, IOError):
-				pass
-			KApplication.kApplication().processEvents()
-			time.sleep(0.0000001)			
-		self.progress.setValue(100)
-    		self.parent.closeButton.setEnabled(True)
-		
+                try:
+                    readable = select.select([rNum],[],[],0.001)
+                    if len(readable[0]) > 0:
+                        self.exitstatus = int(os.read(rNum,2))
+
+                except (OSError, IOError):
+                    pass
+                KApplication.kApplication().processEvents()
+                time.sleep(0.0000001)
+            self.progress.setValue(100)
+            self.parent.closeButton.setEnabled(True)
+
 class KDEInstallProgressAdapter(InstallProgress):
     def __init__(self, progress, action, parent):
-	# TODO: implement the term
-	InstallProgress.__init__(self)
-	self.progress = progress
-	self.action = action
-	self.parent = parent
-	self.finished = False
+        # TODO: implement the term
+        InstallProgress.__init__(self)
+        self.progress = progress
+        self.action = action
+        self.parent = parent
+        self.finished = False
 
     def child_exited(self,process):
-	print "processExited(self):"
-	print "exit status: " + str(process.exitStatus())
-	self.finished = True
-	self.apt_status = process.exitStatus()
+        print "processExited(self):"
+        print "exit status: " + str(process.exitStatus())
+        self.finished = True
+        self.apt_status = process.exitStatus()
 
-	#print "child_exited: %s %s %s %s" % (self,term,pid,status)
-	#se lf.apt_status = posix.WEXITSTATUS(status)
-	self.finished = True
+        #print "child_exited: %s %s %s %s" % (self,term,pid,status)
+        #se lf.apt_status = posix.WEXITSTATUS(status)
+        self.finished = True
     def error(self, pkg, errormsg):
-	# FIXME: display a msg
-	#self.term_expander.set_expanded(True)
-	pass
+        # FIXME: display a msg
+        #self.term_expander.set_expanded(True)
+        pass
     def conffile(self, current, new):
-	# FIXME: display a msg or expand term
-	#self.term_expander.set_expanded(True)
-	pass
+        # FIXME: display a msg or expand term
+        #self.term_expander.set_expanded(True)
+        pass
     def startUpdate(self):
-	#print "startUpdate"
-	apt_pkg.PkgSystemUnLock()
-	self.action.setText(_("Installing dependencies..."))
-	self.progress.setValue(0)
+        #print "startUpdate"
+        apt_pkg.PkgSystemUnLock()
+        self.action.setText(_("Installing dependencies..."))
+        self.progress.setValue(0)
     def statusChange(self, pkg, percent, status):
-	self.progress.setValue(percent)
-	print status # mhb debug
-	#self.progress.setText(status)
+        self.progress.setValue(percent)
+        print status # mhb debug
+        #self.progress.setText(status)
     def finishUpdate(self):
-	#print "finished"
-	pass
+        #print "finished"
+        pass
     def updateInterface(self):
         # log the output of dpkg (on the master_fd) to the terminal log
         while True:
@@ -181,15 +179,15 @@ class KDEInstallProgressAdapter(InstallProgress):
                 print e
                 #logging.debug("error reading from self.master_fd '%s'" % e)
                 break
-	try:
-	    InstallProgress.updateInterface(self)
-	except ValueError,e:
-	    pass
-	
-	KApplication.kApplication().processEvents()
-	time.sleep(0.0000001)
-	#while gtk.events_pending():
-	#gtk.main_iteration()
+        try:
+            InstallProgress.updateInterface(self)
+        except ValueError,e:
+            pass
+
+        KApplication.kApplication().processEvents()
+        time.sleep(0.0000001)
+        #while gtk.events_pending():
+        #gtk.main_iteration()
 
     def fork(self):
         """pty voodoo"""
@@ -217,42 +215,42 @@ class KDEInstallProgressAdapter(InstallProgress):
 
 class KDEFetchProgressAdapter(apt.progress.FetchProgress):
     def __init__(self,progress,label,parent):
-	self.progress = progress
-	self.label = label
-	self.parent = parent
+        self.progress = progress
+        self.label = label
+        self.parent = parent
     def start(self):
-	#print "start()"
-	self.label.setText(_("Downloading additional package files..."))
-	self.progress.setValue(0)
+        #print "start()"
+        self.label.setText(_("Downloading additional package files..."))
+        self.progress.setValue(0)
     def stop(self):
-	#print "stop()"
-	pass
+        #print "stop()"
+        pass
     def pulse(self):
-	apt.progress.FetchProgress.pulse(self)
+        apt.progress.FetchProgress.pulse(self)
         self.progress.setValue(self.percent)
         currentItem = self.currentItems + 1
         if currentItem > self.totalItems:
             currentItem = self.totalItems
-	if self.currentCPS > 0:
-	    self.label.setText(_("Downloading additional package files...") + _("File %s of %s at %sB/s" % (self.currentItems,self.totalItems,apt_pkg.SizeToStr(self.currentCPS))))
-	else:
-	    self.label.setText(_("Downloading additional package files...") + _("File %s of %s" % (self.currentItems,self.totalItems)))
-	KApplication.kApplication().processEvents()
-	return True
+        if self.currentCPS > 0:
+            self.label.setText(_("Downloading additional package files...") + _("File %s of %s at %sB/s" % (self.currentItems,self.totalItems,apt_pkg.SizeToStr(self.currentCPS))))
+        else:
+            self.label.setText(_("Downloading additional package files...") + _("File %s of %s" % (self.currentItems,self.totalItems)))
+        KApplication.kApplication().processEvents()
+        return True
     def mediaChange(self, medium, drive):
-	#FIXME test
+    #FIXME test
         msg = _("Please insert '%s' into the drive '%s'") % (medium,drive)
         change = QMessageBox.question(None, _("Media Change"), msg, QMessageBox.Ok, QMessageBox.Cancel)
         if change == QMessageBox.Ok:
             return True
         return False
-	
+
 class CacheProgressAdapter(apt.progress.FetchProgress):
     def __init__(self, progressbar):
-	self.progressbar = progressbar
+        self.progressbar = progressbar
     def update(self, percent):
-	self.progressbar.show()
-	self.progressbar.setValue(percent)
+        self.progressbar.show()
+        self.progressbar.setValue(percent)
     def done(self):
-	pass
+        pass
 
