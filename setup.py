93d9ff31c1edc132191796af04ed4736b474e137
Luca Falavigna <dktrkranz@debian.org>
2011-08-28 16:22:30 +0200

    Make buildsystem a little less noisy

--- setup.py
+++ setup.py
@@ -1,51 +1,49 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
-import glob
 import os
-import re
+from distutils.core import setup
+from glob import glob
+from re import compile
 
-# look/set what version we have
-changelog = "debian/changelog"
+GETTEXT_NAME='gdebi'
+I18NFILES = []
+
+# Look/set what version we have
+changelog = 'debian/changelog'
 if os.path.exists(changelog):
     head=open(changelog).readline()
-    match = re.compile(".*\((.*)\).*").match(head)
+    match = compile('.*\((.*)\).*').match(head)
     if match:
         version = match.group(1)
-        f=open("GDebi/Version.py","w")
-        f.write("VERSION=\"%s\"\n" % version)
+        f=open('GDebi/Version.py', 'w')
+        f.write('VERSION="%s"\n' % version)
         f.close()
 
-
-GETTEXT_NAME="gdebi"
-I18NFILES = []
-for filepath in glob.glob("po/mo/*/LC_MESSAGES/*.mo"):
-    lang = filepath[len("po/mo/"):]
-    targetpath = os.path.dirname(os.path.join("share/locale",lang))
+for filepath in glob('po/mo/*/LC_MESSAGES/*.mo'):
+    lang = filepath[len('po/mo/'):]
+    targetpath = os.path.dirname(os.path.join('share/locale', lang))
     I18NFILES.append((targetpath, [filepath]))
 
-# we should probably only run those os.system() stuff when "build" is
-# the cmndline argument
-os.system("intltool-merge -d po data/gdebi.desktop.in"\
-                       " build/gdebi.desktop")
-os.system("intltool-merge -d po data/gdebi-kde.desktop.in"\
-                       " build/gdebi-kde.desktop")
+s = setup(name='gdebi',
+          version=version,
+          packages=['GDebi'],
+          scripts=['gdebi', 'gdebi-gtk', 'gdebi-kde'],
+          data_files=[('share/gdebi/',
+                       ['data/gdebi.ui', 'data/GDebiKDEDialog.ui',
+                        'data/GDebiKDEInstallDialog.ui']),
+                      ('share/applications',
+                       ['build/gdebi.desktop']),
+                      ('share/applications/kde4',
+                       ['build/gdebi-kde.desktop']),
+                      ('share/application-registry',
+                       ['data/gdebi.applications']),
+                      ('share/gdebi/',
+                       ['data/gdebi.png'])] + I18NFILES)
 
-# HACK: make sure that the mo files are generated and up-to-date
-os.system("cd po; make update-po")
-    
-setup(name='gdebi',
-      version=version,
-      packages=['GDebi'],
-      scripts=['gdebi','gdebi-gtk','gdebi-kde'],
-      data_files=[('share/gdebi/',
-                   ["data/gdebi.ui", "data/GDebiKDEDialog.ui", "data/GDebiKDEInstallDialog.ui"]),
-                  ('share/applications',
-                   ["build/gdebi.desktop"]),
-		  ('share/applications/kde4',
-		   ["build/gdebi-kde.desktop"]),
-                  ('share/application-registry',
-		   ["data/gdebi.applications"]),
-                  ('share/gdebi/',
-                   ["data/gdebi.png"])]+I18NFILES,
-      )
+# Make sure that the mo files are generated and up-to-date
+if 'build' in s.commands:
+    os.system('intltool-merge -d po data/gdebi.desktop.in'\
+              ' build/gdebi.desktop')
+    os.system('intltool-merge -d po data/gdebi-kde.desktop.in'\
+              ' build/gdebi-kde.desktop')
+    os.system('make -C po update-po')
