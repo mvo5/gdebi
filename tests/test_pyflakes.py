792392acbc7f8831b54326ce6d79f888d969c01e
Dimitri John Ledkov <dimitri.ledkov@canonical.com>
2014-02-28 22:50:28 +0000

    * Add top-level executable scripts to pyflakes test. * Check using both pyflakes and pyflakes3.

--- tests/test_pyflakes.py
+++ tests/test_pyflakes.py
@@ -7,8 +7,14 @@ class TestPyflakesClean(unittest.TestCase):
     """ ensure that the tree is pyflakes clean """
 
     def test_pyflakes_clean(self):
-        path = os.path.join(os.path.dirname(__file__), "..")
-        self.assertEqual(subprocess.call(["pyflakes",  path]), 0)
+        paths = [
+            os.path.join(os.path.dirname(__file__), ".."),
+            os.path.join(os.path.dirname(__file__), "..", "gdebi"),
+            os.path.join(os.path.dirname(__file__), "..", "gdebi-gtk"),
+            os.path.join(os.path.dirname(__file__), "..", "gdebi-kde"),
+            ]                                    
+        self.assertEqual(subprocess.call(["pyflakes",] +  paths), 0)
+        self.assertEqual(subprocess.call(["pyflakes3",] +  paths), 0)
 
 
 if __name__ == "__main__":
