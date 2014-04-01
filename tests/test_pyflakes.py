1351a63437faa54f4b7cd01d151af83b91ed7591
Barry Warsaw <barry@python.org>
2014-04-01 12:08:03 -0400

    * New upstream release. * Switched all command line scripts to Python 3 and dropped Dependencies   on Python 3.  (LP: #1283574) * Some whitespace cleanup. * Removed some unnecessary shebang lines.

--- tests/test_pyflakes.py
+++ tests/test_pyflakes.py
@@ -1,20 +1,29 @@
 import os
+import locale
 import subprocess
 import unittest
+
+# LP: #1299169
+IN_UTF8 = 'utf-8' in locale.getpreferredencoding().lower()
 
 
 class TestPyflakesClean(unittest.TestCase):
     """ ensure that the tree is pyflakes clean """
 
-    def test_pyflakes_clean(self):
-        paths = [
+    def setUp(self):
+        self.paths = [
             os.path.join(os.path.dirname(__file__), ".."),
             os.path.join(os.path.dirname(__file__), "..", "gdebi"),
             os.path.join(os.path.dirname(__file__), "..", "gdebi-gtk"),
             os.path.join(os.path.dirname(__file__), "..", "gdebi-kde"),
-            ]                                    
-        self.assertEqual(subprocess.call(["pyflakes",] +  paths), 0)
-        self.assertEqual(subprocess.call(["pyflakes3",] +  paths), 0)
+            ]
+
+    def test_pyflakes_clean(self):
+        self.assertEqual(subprocess.check_call(['pyflakes'] + self.paths), 0)
+
+    @unittest.skipUnless(IN_UTF8, 'LP: #1299169')
+    def test_pyflakes3_clean(self):
+        self.assertEqual(subprocess.check_call(['pyflakes3'] +  self.paths), 0)
 
 
 if __name__ == "__main__":
